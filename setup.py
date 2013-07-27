430d40bdeb0702080876d93feb89a4e44f889aef
Judge Maygarden <jmaygarden@gmail.com>
2013-07-27 14:14:05 -0500

    Added a setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ftl',
+      version='0.1.0',
+      description='This is a collection of Python scripts for dealing with Faster Than Light profiles.',
+      author='Judge Maygarden',
+      author_email='jmaygarden+ftl@gmail.com',
+      url='https://github.com/jmaygarden/ftl-tools',
+      packages=['ftl'],
+      install_requires=[])
+
